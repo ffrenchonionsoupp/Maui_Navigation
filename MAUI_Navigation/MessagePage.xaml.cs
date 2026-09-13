5ed55f423d67c92bbfc53402368a94c6f637f60c
Francis Hampton <fraham5822@students.ecpi.edu>
2026-09-12 23:20:53 -0400

    Added message input and page navigation

---
+++ MAUI_Navigation/MessagePage.xaml.cs
@@ -0,0 +1,21 @@
+namespace MAUI_Navigation;
+
+public partial class MessagePage : ContentPage
+{
+	public MessagePage(string message)
+	{
+		InitializeComponent();
+		if (!string.IsNullOrEmpty(message))
+		{
+			lblMessage.Text = message;
+		}
+		else
+		{
+			lblMessage.Text = "No message received.";
+        }
+    }
+	private void OnGoBackClicked(object sender, EventArgs e)
+	{
+		Navigation.PopAsync();
+    }
+}
---
+++ MAUI_Navigation/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUI_Navigation
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
